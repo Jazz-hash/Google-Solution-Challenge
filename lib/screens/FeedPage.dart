0a7c464668330a8f297c80c03e7536645c34784c
nj04707 <nj04707@st.habib.edu.pk>
2021-03-29 18:36:36 +0500

    main feed page added

---
+++ lib/screens/FeedPage.dart
@@ -0,0 +1,73 @@
+import 'package:bizzhome/app.dart';
+import 'package:bizzhome/widgets/imageBanner.dart';
+import 'package:bizzhome/widgets/FeedTileOverlay.dart';
+import 'package:flutter/material.dart';
+import 'package:bizzhome/models/Task.dart';
+
+class FeedPage extends StatelessWidget {
+  _onTaskTap(BuildContext context, int taskID) {
+    Navigator.pushNamed(context, TaskDetailRoute, arguments: {"id": taskID});
+  }
+
+  _onTaskTypeTap(BuildContext context, String type) {
+    final tasks = Task.fetchInProgress(type);
+    for (var i = 0; i < tasks.length; i++) {
+      debugPrint(tasks[i].title);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tasks = Task.fetchAllavailable();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Tasks"),
+        backgroundColor: Colors.black,
+        brightness: Brightness.light,
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) =>
+                      _itemBuilder(context, tasks[index]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _itemBuilder(BuildContext context, task) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: GestureDetector(
+        onTap: () => _onTaskTap(context, task.id),
+        key: Key('task_list_item_${task.id}'),
+        child: Container(
+          height: 245.0,
+          child: Stack(
+            children: [
+              ImageBanner(assetPath: task.imagePath),
+              FeedTileOverlay(
+                task.title,
+                task.assignedDate,
+                task.description,
+                task.duration,
+                task.points,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
